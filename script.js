85ec46f70781b890993d5027111ce12b51f693de
Dionys <dionys.colson@laposte.net>
2023-12-08 00:59:39 +0100

    translateX pour passer de gauche à droite
    letsgoo

---
+++ script.js
@@ -0,0 +1,7 @@
+function transitionToRight() {
+    document.querySelector('.container').style.transform = 'translateX(-50%)';
+}
+
+function transitionToLeft() {
+    document.querySelector('.container').style.transform = 'translateX(0)';
+}
